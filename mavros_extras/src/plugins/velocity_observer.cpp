60a2645abf0b6664ff6e3f8a99ff09c1bf7f7806
David Akre <dakre21@gmail.com>
2025-03-25 10:28:56 -0600

    Adding velocity observer [p,q,r,u,v,w] plugin

---
+++ mavros_extras/src/plugins/velocity_observer.cpp
@@ -0,0 +1,64 @@
+/*
+ * TODO - BPRL CU Boulder
+ */
+/**
+ * @brief Velocity Observer plugin
+ * @file bprl_velocity_observer.cpp
+ * @author David Akre
+ *
+ * @addtogroup plugin
+ * @{
+ */
+
+#include "geometry_msgs/msg/twist_stamped.hpp"
+#include "mavros/mavros_uas.hpp"
+#include "mavros/plugin.hpp"
+#include "mavros/plugin_filter.hpp"
+
+namespace mavros {
+namespace extra_plugins {
+class VelocityObserverPlugin : public plugin::Plugin {
+ public:
+  explicit VelocityObserverPlugin(plugin::UASPtr uas_)
+      : Plugin(uas_, "velocity_observer") {
+    velocity_sub_ = node->create_subscription<geometry_msgs::msg::TwistStamped>(
+        "~/velocity", 10,
+        std::bind(&VelocityObserverPlugin::velocity_cb, this,
+                  std::placeholders::_1));
+  }
+
+  Subscriptions get_subscriptions() override { return {/* Rx disabled */}; }
+
+ private:
+  rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr
+      velocity_sub_;
+
+  void send_velocity_estimate(const uint64_t usec, const Eigen::VectorXd& v,
+                              const Eigen::Vector3d w) {
+    mavlink::common::msg::ODOMETRY odom{};
+
+    // (dakre) make this unique from odom plugin
+    odom.frame_id = 99;
+    odom.time_usec = usec;
+    odom.vx = v.x();
+    odom.vy = v.y();
+    odom.vz = v.z();
+    odom.rollspeed = w.x();
+    odom.pitchspeed = w.y();
+    odom.yawspeed = w.z();
+
+    std::cout << "SENDING ODOM MESSAGE" << std::endl;
+    uas->send_message(odom);
+  }
+
+  void velocity_cb(const geometry_msgs::msg::TwistStamped::SharedPtr msg) {
+    send_velocity_estimate(get_time_usec(msg->header.stamp),
+                           ftf::to_eigen(msg->twist.linear),
+                           ftf::to_eigen(msg->twist.angular));
+  }
+};
+}  // namespace extra_plugins
+}  // namespace mavros
+
+#include <mavros/mavros_plugin_register_macro.hpp>
+MAVROS_PLUGIN_REGISTER(mavros::extra_plugins::VelocityObserverPlugin)
